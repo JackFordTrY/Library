fcc0cf2fa1a1cb8c0a4d5982a06f9b4147cc9fd3
JackFordTrY <jackford35241@gmail.com>
2023-03-06 13:10:55 +0200

    Add project files.

---
+++ Library.WebUI/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.Run();
