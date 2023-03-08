6e5089f51ee6ad72687015ee6076a8408b663789
JackFordTrY <jackford35241@gmail.com>
2023-03-08 13:35:44 +0200

    Update Database

--- Library.Persistence/Context/ApplicationDbContext.cs
+++ Library.Persistence/Context/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ public class ApplicationDbContext: DbContext
 
     public DbSet<Book> Books { get; set; }
 
-    public DbSet<Author> Author { get; set; }
+    public DbSet<Author> Authors { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
