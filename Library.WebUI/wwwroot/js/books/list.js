6ac4eaa7cc9d705b4d29d5ca938fd64c56669780
JackFordTrY <jackford35241@gmail.com>
2023-03-31 15:47:53 +0300

    Add filters to books list

--- Library.WebUI/wwwroot/js/books/list.js
+++ Library.WebUI/wwwroot/js/books/list.js
@@ -1,17 +1,95 @@
-let curPage = 1;
-let isAvailable = false
+let currentPage = 1;
+
+let isAvailable = true;
 
 let gridContainer;
 
 const gridWrap = document.getElementById("gridwrap");
 
+const filterContainer = document.querySelector(".filter-container");
+
+const dropdownItems = document.querySelectorAll(".dropdown-item");
+
+const applyButton = document.querySelector(".filter-button.apply");
+
+const formData = new FormData();
+
 window.onload = loadList();
 
+dropdownItems.forEach(b => b.addEventListener("click", e => e.stopPropagation()));
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight * 0.85
+        && isAvailable
+    )
+    {
+        console.log("scroll fired");
+        isAvailable = !isAvailable;
+
+        formData.set("page", currentPage);
+
+        fetch("/list", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.hasNextPage) {
+                currentPage++;
+                isAvailable = !isAvailable;
+            }
+            updateList(data.books);
+        });
+    }
+});
+
+applyButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    currentPage = 1;
+
+    formData.set("page", currentPage);
+
+    if (document.querySelector("input[name=\"order\"]:checked")) {
+        formData.set("order", document.querySelector("input[name=\"order\"]:checked").value);
+    }
+
+    if (document.querySelector("input[name=\"direction\"]:checked")) {
+        formData.set("direction", document.querySelector("input[name=\"direction\"]:checked").value);
+    }
+
+    if (!isNaN(document.querySelector("input[name=\"yearFrom\"]").value)) {
+        formData.set("yearFrom", document.querySelector("input[name=\"yearFrom\"]").value);
+    }
+
+    if (!isNaN(document.querySelector("input[name=\"yearTo\"]").value)) {
+        formData.set("yearTo", document.querySelector("input[name=\"yearTo\"]").value);
+    }
+
+    let genres = [];
+
+    document.querySelectorAll("input[name=\"genre\"]:checked").forEach((e)=> genres.push(+e.value));
+
+    if (genres.length > 0) {
+        formData.set("genreFilters", JSON.stringify(genres));
+    }
+
+    fetch("/list?", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data=>{
+        if (data.hasNextPage) {
+            currentPage++;
+            isAvailable = true;
+        }
+        updateList(data.books, currentPage - 1 == 1);
+    })
+});
 
 function loadList() {
-
-
-    if (curPage == 1) {
+    if (currentPage == 1) {
         let loader =
         `
             <span class="loader"></span>
@@ -20,49 +98,32 @@ function loadList() {
         gridWrap.innerHTML = loader;
     }
 
+    formData.set("page", currentPage)
 
-    fetch("/list?" + new URLSearchParams({
-        page: curPage
-    }))
+    formData.set("order", "Title")
+
+    formData.set("direction", "Ascending")
+
+    fetch("/list?", {
+        method: "POST",
+        body: formData
+    })
     .then(response => response.json())
     .then(data => {
-        gridWrap.innerHTML = 
+        gridWrap.innerHTML =
         `
             <div class="grid-container" id="gridcontainer"></div>
         `;
         if (data.hasNextPage) {
-            curPage++;
+            currentPage++;
         }
         gridContainer = document.getElementById("gridcontainer")
         updateList(data.books)
     });
 };
 
-window.addEventListener("scroll", () => {
-    console.log("scroll");
 
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight * 0.9
-        && !isAvailable
-    )
-    {
-        isAvailable = !isAvailable;
-
-        fetch("/list?" + new URLSearchParams({
-            page: curPage
-        }))
-        .then(response => response.json())
-        .then(data => {
-            if (data.hasNextPage) {
-                curPage++;
-                isAvailable = !isAvailable;
-            }
-            updateList(data.books);
-        });
-    }
-});
-
-function updateList(books){
-
+function updateList(books, rerender = false) {
     const booksHtml = books.map(b =>
         `
             <div class="grid-item">
@@ -70,9 +131,15 @@ function updateList(books){
                 <br>
                 <a class="grid-item-text-link" href="/${b.title.toLowerCase()}" aria-label="${b.title}">${b.title}</a>
                 <br>
-                <p style="color:black;">${b.date}</p>
+                <p style="color:white;">${b.date}</p>
             </div>
-    `).join('');
+        `
+    ).join('');
 
-    gridContainer.innerHTML += booksHtml;    
+    if (rerender) {
+        gridContainer.innerHTML = booksHtml;
+    }
+    else {
+        gridContainer.innerHTML += booksHtml;
+    }
 }
